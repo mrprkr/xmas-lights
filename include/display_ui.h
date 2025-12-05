63d100f6428e87bc37957e737c518807b13ff7ca
Michael Parker <michael@stuntdouble.io>
2025-12-05 14:33:44 +1100

    improved layout and color options

--- include/display_ui.h
+++ include/display_ui.h
@@ -7,13 +7,18 @@
 #include "calibration.h"
 #include "wifi_manager.h"
 
+// Tab indices
+enum TabIndex {
+    TAB_CONTROL = 0,
+    TAB_CALIBRATE = 1,
+    TAB_SETTINGS = 2
+};
+
 class DisplayUI {
 public:
     DisplayUI(LEDController& ledController, Calibration& calibration, WiFiManager& wifiManager);
     void begin();
     void update();
-
-    // Sync UI with current state
     void syncState();
 
 private:
@@ -21,69 +26,86 @@ private:
     Calibration& _calibration;
     WiFiManager& _wifiManager;
 
-    // Screen objects
-    lv_obj_t* _tabview;
-    lv_obj_t* _tabControl;
-    lv_obj_t* _tabCalibrate;
-    lv_obj_t* _tabSettings;
+    // Main containers
+    lv_obj_t* _screen;
+    lv_obj_t* _tabBar;
+    lv_obj_t* _contentArea;
+    lv_obj_t* _tabBtns[3];
+    int _activeTab;
 
-    // Control tab widgets
-    lv_obj_t* _powerSwitch;
+    // Control tab - bento grid
+    lv_obj_t* _controlContainer;
+    lv_obj_t* _powerTile;
+    lv_obj_t* _powerIcon;
+    lv_obj_t* _patternTile;
+    lv_obj_t* _patternValue;
+    lv_obj_t* _brightnessTile;
+    lv_obj_t* _brightnessValue;
+    lv_obj_t* _colorTile;
+    lv_obj_t* _colorSwatch;
+
+    // Pattern selector overlay
+    lv_obj_t* _patternOverlay;
+    lv_obj_t* _patternList;
+
+    // Brightness overlay
+    lv_obj_t* _brightnessOverlay;
     lv_obj_t* _brightnessSlider;
-    lv_obj_t* _brightnessLabel;
-    lv_obj_t* _colorWheel;
-    lv_obj_t* _animBtnMatrix;
-    lv_obj_t* _speedSlider;
-    lv_obj_t* _speedLabel;
+    lv_obj_t* _brightnessDisplay;
 
-    // Calibration tab widgets
-    lv_obj_t* _calibLedLabel;
+    // Color overlay
+    lv_obj_t* _colorOverlay;
+    lv_obj_t* _colorBtns[9];
+
+    // Calibrate tab
+    lv_obj_t* _calibContainer;
+    lv_obj_t* _calibLedValue;
     lv_obj_t* _calibPrevBtn;
     lv_obj_t* _calibNextBtn;
-    lv_obj_t* _calibStartBtn;
     lv_obj_t* _calibXSlider;
     lv_obj_t* _calibYSlider;
     lv_obj_t* _calibZSlider;
-    lv_obj_t* _calibPosLabel;
     lv_obj_t* _calibSaveBtn;
-    lv_obj_t* _calibExitBtn;
-
-    // Settings tab widgets
-    lv_obj_t* _wifiStatusLabel;
-    lv_obj_t* _ipLabel;
-
-    // Current calibration LED
     int16_t _currentCalibLed;
 
+    // Settings tab
+    lv_obj_t* _settingsContainer;
+    lv_obj_t* _wifiIcon;
+    lv_obj_t* _ipValue;
+
     // Build UI
+    void createTabBar();
     void createControlTab();
     void createCalibrateTab();
     void createSettingsTab();
-    void createStyles();
+    void createPatternOverlay();
+    void createBrightnessOverlay();
+    void createColorOverlay();
+    void switchTab(int tab);
+    void showOverlay(lv_obj_t* overlay);
+    void hideOverlay(lv_obj_t* overlay);
 
-    // Event handlers (static for LVGL callbacks)
-    static void onPowerSwitch(lv_event_t* e);
+    // Event handlers
+    static void onTabSelect(lv_event_t* e);
+    static void onPowerTap(lv_event_t* e);
+    static void onPatternTap(lv_event_t* e);
+    static void onBrightnessTap(lv_event_t* e);
+    static void onColorTap(lv_event_t* e);
+    static void onOverlayClose(lv_event_t* e);
+    static void onPatternSelect(lv_event_t* e);
     static void onBrightnessChange(lv_event_t* e);
-    static void onColorChange(lv_event_t* e);
-    static void onAnimationSelect(lv_event_t* e);
-    static void onSpeedChange(lv_event_t* e);
+    static void onColorSelect(lv_event_t* e);
     static void onCalibPrev(lv_event_t* e);
     static void onCalibNext(lv_event_t* e);
-    static void onCalibStart(lv_event_t* e);
-    static void onCalibExit(lv_event_t* e);
-    static void onCalibSave(lv_event_t* e);
-    static void onCalibReset(lv_event_t* e);
     static void onCalibPosChange(lv_event_t* e);
-    static void onTabChange(lv_event_t* e);
+    static void onCalibSave(lv_event_t* e);
 
-    // Helper methods
+    // Helpers
+    void updateControlTiles();
     void updateCalibrationUI();
     void updateWifiStatus();
-    lv_color_t crgbToLvColor(CRGB color);
-    CRGB lvColorToCrgb(lv_color_t color);
 };
 
-// Global UI instance
 extern DisplayUI* displayUI;
 
-#endif // DISPLAY_UI_H
+#endif
